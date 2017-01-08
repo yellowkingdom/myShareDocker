fdf0241c982c9dff985f53f8109e9afaa91e896e
yellow kingdom <yellowkingdom@live.cn>
2017-01-08 19:44:07 +0800

    use django

--- run.sh
+++ run.sh
@@ -15,3 +15,8 @@ cat $KEYFILE
 chmod 600 /root/.ssh/authorized_keys
 
 /usr/sbin/sshd -D -f /app/sshrun/sshd_config
+
+mkdir -p /root/web/ && cd /root/web/
+django-admin startproject mysharesite
+cd /root/web/mysharesite
+python manage.py runserver 0.0.0.0:8000
