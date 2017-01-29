6f21314ede7cea27f34e1a5522c2d1640346bf69
yellowkingdom <yellowkingdom@live.cn>
2017-01-29 15:17:20 +0800

    try to use django develop&test command instead of nginx&uwsgi

--- run.sh
+++ run.sh
@@ -16,8 +16,8 @@ cat $KEYFILE
 chmod 600 /root/.ssh/authorized_keys
 
 # create a project
-cd /app/sshrun
-/usr/bin/python /usr/bin/django-admin.py startproject mysite
+#cd /app/sshrun
+#/usr/bin/python /usr/bin/django-admin.py startproject mysite
 
 # setup uwsgi dependences
 #mkdir -p /var/log/uwsgi/
@@ -26,14 +26,15 @@ cd /app/sshrun
 #/usr/sbin/uwsgi --ini /app/sshrun/uwsgi.ini
 
 # nginx settings
-#adduser -D -u 1000 -g 'www' www
-#cp -f /app/sshrun/nginx.conf /etc/nginx/nginx.conf
+adduser -D -u 1000 -g 'www' www
+cp -f /app/sshrun/nginx.conf /etc/nginx/nginx.conf
 
 #test
 #chown -R www:www /var/lib/nginx
 #chown -R www:www /app/sshrun/mysite
-#mkdir -p /run/nginx/
+#mkdir -p /var/run/
+#touch /var/run/nginx.pid
 
 #/usr/sbin/nginx -c /etc/nginx/nginx.conf  & /usr/sbin/sshd -D -f /app/sshrun/sshd_config
-echo "ALLOWED_HOSTS = ['*']" >> /app/sshrun/mysite/mysite/settings.py
+#echo "ALLOWED_HOSTS = ['*']" >> /app/sshrun/mysite/mysite/settings.py
 /usr/bin/python /app/sshrun/mysite/manage.py runserver 0.0.0.0:8000 & /usr/sbin/sshd -D -f /app/sshrun/sshd_config
